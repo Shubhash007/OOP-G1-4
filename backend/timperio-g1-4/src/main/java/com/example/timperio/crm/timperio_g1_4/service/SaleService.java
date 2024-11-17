4d0e3da4fc552d9f37b3ca4ca5505bd68fa45207
nicamanas <xyukimiku@gmail.com>
2024-11-17 17:14:00 +0800

    removed deprecated sales classes

--- backend/timperio-g1-4/src/main/java/com/example/timperio/crm/timperio_g1_4/service/SaleService.java
+++
@@ -1,8 +0,0 @@
-package com.example.timperio.crm.timperio_g1_4.service;
-
-import com.example.timperio.crm.timperio_g1_4.dto.SaleDto;
-
-public interface SaleService {
-    
-    SaleDto createProduct(SaleDto saleDto);
-}
